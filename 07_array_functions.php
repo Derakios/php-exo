dc781c82e44c2b2556ca779ede00a8d4f6ac6fec
Derakios <kiki56860@gmail.com>
2024-06-03 15:42:59 +0200

    Modif arrays

--- 07_array_functions.php
+++ 07_array_functions.php
@@ -23,4 +23,46 @@ unset($fruits[0]);
 $chunkedArray = array_chunk($fruits, 3);
 var_dump($chunkedArray);
 
+//combine array
+$a = ['a', 'b', 'c'];
+$b = ['avocado', 'banana', 'cherry'];
+$c = array_combine($a, $b);
+
+var_dump($c);
+
+echo $c['b'];
+
+$keys = array_keys($c);
+
+echo '<br><br>';
+
+var_dump($keys);
+
+echo '<br><br>';
+
+$flipped = array_flip($c);
+var_dump($flipped);	
+
+echo '<br><br>';
+
+$numbers = range(1, 10);
+var_dump($numbers);
+
+echo '<br><br>';
+
+$newNumbers = array_map(function ($number) {
+    return "Number $number";
+}, $numbers);
+
+var_dump($newNumbers);
+
+echo '<br><br>';
+
+$lessThan10 = array_filter($numbers, fn($number) => $number < 10);
+
+// Array Reduce
+// "carry" holds the return value of the previous iteration
+$sum = array_reduce($numbers, fn($carry, $number) => $carry + $number);
+var_dump($sum);
+
 ?>
